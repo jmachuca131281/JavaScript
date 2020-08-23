6d7dede24592c7f4447f4d10ddc73cb97e1a1d99
jmachuca131281 <jmachucaprogrammer@gmail.com>
2020-08-23 09:36:54 -0500

    Added DOM pdf file

--- Domina el DOM con JS/Sección 1 introducción al estudio del DOM/ejerciciosDOM/ejerciciosDOM/01-Introduccion/js/fotos02.js
+++ Domina el DOM con JS/Sección 1 introducción al estudio del DOM/ejerciciosDOM/ejerciciosDOM/01-Introduccion/js/fotos02.js
@@ -1,25 +1,28 @@
-window.onload = function(){
+window.onload = function() {
+
 	console.time("tiempo");
 	//Recuperamos las fotos dentro de la división
 	var fotos = document.querySelector("div");
 	console.dir(document.getElementsByTagName("img"));
-	console.info("Hola, cara de bola");
-	console.warn("Hola, cara de bola");
-	console.error("Hola, cara de bola");
-	console.log("Hola, cara de bola");
-	console.group("Imagenes");
-		console.dir(document.querySelectorAll("img"));
+	console.info("Info, cara de bola");
+	console.warn("Warn, cara de bola");
+	console.error("Error, cara de bola");
+	console.log("Log, cara de bola");
+
+	console.group("Imagenes desde el group");
+  console.dir(document.querySelectorAll("img"));
 	console.groupEnd();
-	console.groupCollapsed("Imágenes");
-		console.dir(document.querySelectorAll("img"));
+
+	console.groupCollapsed("Imágenes desde el grupCollapsed");
+  console.dir(document.querySelectorAll("img"));
 	console.groupEnd();
-	
+
 	//Listener
 	fotos.addEventListener("click",function(e){
 		//Igualdad estricta
 		if(e.target.tagName==="IMG"){
 			console.log("Pulso una imagen");
-		} else { 
+		} else {
 			console.log("NO pulso una imagen");
 		}
 	}, false);
