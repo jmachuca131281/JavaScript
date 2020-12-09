7ab43a6cf56c98482395116370c55230d35903e7
jmachuca131281 <jmachucaprogrammer@gmail.com>
2020-12-08 19:31:05 -0500

    Get up file from pc

---
+++ TODO/src/classes/todo-list.class.js
@@ -0,0 +1,24 @@
+/* Todo list va a grupar todas las listas de Todo's */
+
+export class TodoList{
+
+  constructor(){
+    this.todos = [];
+  }
+
+  nuevoTodo (todo){
+    this.todos.push(todo);
+  }
+
+  elimiarTodo(id){
+
+  }
+
+  marcarCompletado(id){
+
+  }
+
+  elimarCompletado(){
+
+  }
+}
---
+++ webpack/src/index.js
@@ -0,0 +1,9 @@
+import { saludar } from './js/componentes.js';
+import './styles.css';
+// import img from './assets/1w.jpeg';
+
+const nombre = 'Jose';
+
+saludar (nombre);
+
+
